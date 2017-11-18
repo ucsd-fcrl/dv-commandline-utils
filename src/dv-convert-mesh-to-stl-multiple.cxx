543c7b12a0b62703db96f96bdd58cb9f9c0d6113
Francisco Contijoch <fcontijoch@Franciscos-MacBook-Pro.local>
2017-11-17 21:57:40 -0800

    FC made two label mesher

--- src/dv-convert-mesh-to-stl-multiple.cxx
+++ src/dv-convert-mesh-to-stl-multiple.cxx
@@ -8,6 +8,8 @@
 #include <itkSTLMeshIO.h>
 #include <itkBinaryMask3DMeshSource.h>
 #include <itkImage.h>
+#include <itkAddImageFilter.h>
+#include <itkBinaryThresholdImageFilter.h>
 
 const unsigned int Dimension = 3;
 typedef float      TCoordinate;
@@ -21,13 +23,17 @@ typedef itk::BinaryMask3DMeshSource< ImageType, MeshType >   MeshSourceType;
 typedef itk::Mesh< TCoordinate, Dimension > TMesh;
 typedef itk::MeshFileWriter< TMesh >        TWriter;
 
+typedef itk::BinaryThresholdImageFilter<ImageType, ImageType> TFilter;
+typedef itk::AddImageFilter <ImageType, ImageType > AddImageFilterType;
+
+
 int main( int argc, char* argv[] )
 {
 
-  if ( 4 != argc )
+  if ( 5 < argc )
     {
     std::cerr << "Usage:\n"<< std::endl;
-    std::cerr << argv[0] << " <InputImage> <OutputMesh> <LabelValue>" << std::endl;
+    std::cerr << argv[0] << " <InputImage> <OutputMesh> <LabelValue1> <LabelValue2>" << std::endl;
     return EXIT_FAILURE;
     }
 
@@ -53,13 +59,36 @@ int main( int argc, char* argv[] )
       return EXIT_FAILURE;
       }
 
+      //
+      // Filtering
+      //
+
+      const auto filter1 = TFilter::New();
+      filter1->SetInput(reader->GetOutput());
+      filter1->SetInsideValue( 1 );
+      filter1->SetOutsideValue( 0 );
+      filter1->SetLowerThreshold( std::atof( argv[3] ) );
+      filter1->SetUpperThreshold( std::atof( argv[3] ) );
+
+      const auto filter2 = TFilter::New();
+      filter2->SetInput(reader->GetOutput());
+      filter2->SetInsideValue( 1 );
+      filter2->SetOutsideValue( 0 );
+      filter2->SetLowerThreshold( std::atof( argv[4] ) );
+      filter2->SetUpperThreshold( std::atof( argv[4] ) );
+
+
+      AddImageFilterType::Pointer addFilter  = AddImageFilterType::New ();
+      addFilter->SetInput1(filter1->GetOutput());
+      addFilter->SetInput2(filter2->GetOutput());
+
 //
 //Meshing
 //
 MeshSourceType::Pointer meshSource = MeshSourceType::New();
-const PixelType objectValue = static_cast<PixelType>( std::atof( argv[3] ) );
+const PixelType objectValue = static_cast<PixelType>( 1 );
 meshSource->SetObjectValue( objectValue );
-meshSource->SetInput( reader->GetOutput() );
+meshSource->SetInput( addFilter->GetOutput() );
 
 try
     {
