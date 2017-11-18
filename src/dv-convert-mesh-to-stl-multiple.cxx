ff3a1d0e3c839d7bf3109b270ef527d809204676
Francisco Contijoch <fcontijoch@Franciscos-MacBook-Pro.local>
2017-11-18 00:14:38 -0800

    FC came up with 3 label approach

--- src/dv-convert-mesh-to-stl-multiple.cxx
+++ src/dv-convert-mesh-to-stl-multiple.cxx
@@ -26,16 +26,15 @@ typedef itk::MeshFileWriter< TMesh >        TWriter;
 typedef itk::BinaryThresholdImageFilter<ImageType, ImageType> TFilter;
 typedef itk::AddImageFilter <ImageType, ImageType > AddImageFilterType;
 
-
 int main( int argc, char* argv[] )
 {
 
-  if ( 5 < argc )
-    {
+  if ( 4 > argc )
+  {
     std::cerr << "Usage:\n"<< std::endl;
-    std::cerr << argv[0] << " <InputImage> <OutputMesh> <LabelValue1> <LabelValue2>" << std::endl;
+    std::cerr << argv[0] << " <InputImage> <OutputMesh> <LabelValue1> <LabelValue2> .... <LabelValueN>" << std::endl;
     return EXIT_FAILURE;
-    }
+  }
 
   const std::string inputFileName(argv[1]);
   const std::string outputFileName(argv[2]);
@@ -49,60 +48,79 @@ int main( int argc, char* argv[] )
   reader->SetFileName( inputFileName );
 
   try
-      {
-      reader->Update();
-      }
+  {
+    reader->Update();
+  }
   catch( itk::ExceptionObject & exp )
-      {
-      std::cerr << "Exception thrown while reading the input file " << std::endl;
-      std::cerr << exp << std::endl;
-      return EXIT_FAILURE;
-      }
+  {
+    std::cerr << "Exception thrown while reading the input file " << std::endl;
+    std::cerr << exp << std::endl;
+    return EXIT_FAILURE;
+  }
 
-      //
-      // Filtering
-      //
+  //
+  // Adding Filtering
+  //
 
-      const auto filter1 = TFilter::New();
-      filter1->SetInput(reader->GetOutput());
-      filter1->SetInsideValue( 1 );
-      filter1->SetOutsideValue( 0 );
-      filter1->SetLowerThreshold( std::atof( argv[3] ) );
-      filter1->SetUpperThreshold( std::atof( argv[3] ) );
+  const auto filter1 = TFilter::New();
+  filter1->SetInput(reader->GetOutput());
+  filter1->SetInsideValue( 1 );
+  filter1->SetOutsideValue( 0 );
+  filter1->SetLowerThreshold( std::atof( argv[3] ) );
+  filter1->SetUpperThreshold( std::atof( argv[3] ) );
 
-      const auto filter2 = TFilter::New();
-      filter2->SetInput(reader->GetOutput());
-      filter2->SetInsideValue( 1 );
-      filter2->SetOutsideValue( 0 );
-      filter2->SetLowerThreshold( std::atof( argv[4] ) );
-      filter2->SetUpperThreshold( std::atof( argv[4] ) );
+  const auto filter2 = TFilter::New();
+  filter2->SetInput(reader->GetOutput());
+  filter2->SetInsideValue( 1 );
+  filter2->SetOutsideValue( 0 );
+  filter2->SetLowerThreshold( std::atof( argv[4] ) );
+  filter2->SetUpperThreshold( std::atof( argv[4] ) );
 
+  AddImageFilterType::Pointer addFilter  = AddImageFilterType::New ();
+  addFilter->SetInput1(filter1->GetOutput());
+  addFilter->SetInput2(filter2->GetOutput());
 
-      AddImageFilterType::Pointer addFilter  = AddImageFilterType::New ();
-      addFilter->SetInput1(filter1->GetOutput());
-      addFilter->SetInput2(filter2->GetOutput());
+  addFilter->Update();
 
-//
-//Meshing
-//
-MeshSourceType::Pointer meshSource = MeshSourceType::New();
-const PixelType objectValue = static_cast<PixelType>( 1 );
-meshSource->SetObjectValue( objectValue );
-meshSource->SetInput( addFilter->GetOutput() );
+  std::cerr << "Argc value " << argc <<std::endl;
 
-try
-    {
+  //for (int i = 5; i <= argc; ++i)
+  //{
+    const auto filter3 = TFilter::New();
+    filter3->SetInput(reader->GetOutput());
+    filter3->SetInsideValue( 1 );
+    filter3->SetOutsideValue( 0 );
+    filter3->SetLowerThreshold( std::atof( argv[5] ) );
+    filter3->SetUpperThreshold( std::atof( argv[5] ) );
+
+    addFilter->SetInput1(addFilter->GetOutput());
+    addFilter->SetInput2(filter3->GetOutput());
+
+    addFilter->Update();
+
+  //}
+
+  //
+  //Meshing
+  //
+  MeshSourceType::Pointer meshSource = MeshSourceType::New();
+  const PixelType objectValue = static_cast<PixelType>( 1 );
+  meshSource->SetObjectValue( objectValue );
+  meshSource->SetInput( addFilter->GetOutput() );
+
+  try
+  {
     meshSource->Update();
-    }
+  }
   catch( itk::ExceptionObject & exp )
-    {
+  {
     std::cerr << "Exception thrown during meshSource->Update() " << std::endl;
     std::cerr << exp << std::endl;
     return EXIT_FAILURE;
-    }
+  }
 
-    std::cout << "Nodes = " << meshSource->GetNumberOfNodes() << std::endl;
-    std::cout << "Cells = " << meshSource->GetNumberOfCells() << std::endl;
+  std::cout << "Nodes = " << meshSource->GetNumberOfNodes() << std::endl;
+  std::cout << "Cells = " << meshSource->GetNumberOfCells() << std::endl;
 
   //
   // Writer
@@ -114,20 +132,20 @@ try
 
   const auto o_ext = outputFileName.substr(outputFileName.size() - 3, 3);
   if (o_ext == "stl" || o_ext == "STL")
-    {
+  {
     writer->SetMeshIO( itk::STLMeshIO::New() );
-    }
+  }
 
   try
-    {
+  {
     writer->Update();
-    }
+  }
   catch ( itk::ExceptionObject & err )
-    {
+  {
     std::cerr << "There was a problem writing the file." << std::endl;
     std::cerr << err << std::endl;
     return EXIT_FAILURE;
-    }
+  }
 
   return EXIT_SUCCESS;
 
