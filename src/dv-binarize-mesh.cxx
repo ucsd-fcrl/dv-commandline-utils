54b17cbbc8017674cb78c91b625f7ce1101a40ac
DVigneault <davis.vigneault@gmail.com>
2017-11-21 06:44:07 -0800

    dv-binarize-mesh: Migrate to Boost program options.

--- src/dv-binarize-mesh.cxx
+++ src/dv-binarize-mesh.cxx
@@ -30,33 +30,42 @@ typedef itk::TriangleMeshToBinaryImageFilter< MeshType, OutputImageType > Filter
 
 typedef itk::ImageFileWriter< OutputImageType > WriterType;
 
+namespace po = boost::program_options;
+
 int main(int argc, char** argv)
 {
 
-  if (argc != 4)
+  // Declare the supported options.
+  po::options_description description("Allowed options");
+  description.add_options()
+    ("help", "Print usage information.")
+    ("input-mesh",      po::value<std::string>()->required(), "Filename of the input mesh.")
+    ("reference-image", po::value<std::string>()->required(), "Filename of the reference image.")
+    ("output-image",    po::value<std::string>()->required(), "Filename of the output image.")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, description), vm);
+
+  if (vm.count("help"))
     {
-    std::cerr << "Usage:\n"
-              << argv[0]
-              << " <InputImageName> <InputMeshName> <OutputImageName>"
-              << std::endl;
-    return EXIT_FAILURE;
+    std::cout << description << '\n';
+    return EXIT_SUCCESS;
     }
 
-  const std::string inputImageName  = argv[1];
-  const std::string inputMeshName   = argv[2];
-  const std::string outputImageName = argv[3];
+  po::notify(vm);
 
   const auto meshReader = MeshReaderType::New();
-  meshReader->SetFileName( inputMeshName );
+  meshReader->SetFileName( vm["input-mesh"].as<std::string>() );
 
-  const auto i_ext = inputMeshName.substr(inputMeshName.size() - 3, 3);
+  const auto i_ext = vm["input-mesh"].as<std::string>().substr(vm["input-mesh"].as<std::string>().size() - 3, 3);
   if (i_ext == "stl" || i_ext == "STL")
     {
     meshReader->SetMeshIO( itk::STLMeshIO::New() );
     }
 
   const auto imageReader = ImageReaderType::New();
-  imageReader->SetFileName( inputImageName );
+  imageReader->SetFileName( vm["reference-image"].as<std::string>() );
 
   const auto filter = FilterType::New();
   filter->SetInput( meshReader->GetOutput() );
@@ -74,7 +83,7 @@ int main(int argc, char** argv)
     }
 
   const auto writer = WriterType::New();
-  writer->SetFileName( outputImageName );
+  writer->SetFileName( vm["output-image"].as<std::string>() );
   writer->SetInput( filter->GetOutput() );
 
   try
