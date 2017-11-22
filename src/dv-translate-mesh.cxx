1db5b13925ecbd3ec092708f292895c902a01654
DVigneault <davis.vigneault@gmail.com>
2017-11-22 12:21:35 -0800

    Use boost:program_options

--- src/dv-translate-mesh.cxx
+++ src/dv-translate-mesh.cxx
@@ -24,20 +24,35 @@ int
 main( int argc, char* argv[] )
 {
 
-  if ( 6 != argc )
+  // Declare the supported options.
+  po::options_description description("Allowed options");
+  description.add_options()
+    ("help", "Print usage information.")
+    ("input-mesh",  po::value<std::string>()->required(), "Filename of the input mesh.")
+    ("output-mesh", po::value<std::string>()->required(), "Filename of the output image.")
+    ("x",           po::value<double>()->required(), "Translation in the x direction.")
+    ("y",           po::value<double>()->required(), "Translation in the y direction.")
+    ("z",           po::value<double>()->required(), "Translation in the z direction.")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, description), vm);
+
+  if (vm.count("help"))
     {
-    std::cerr << "Usage: "<< std::endl;
-    std::cerr << argv[0] << " <InputMesh> <OutputMesh> <X> <Y> <Z>" << std::endl;
-    return EXIT_FAILURE;
+    std::cout << description << '\n';
+    return EXIT_SUCCESS;
     }
 
-  const std::string inputFileName(argv[1]);
-  const std::string outputFileName(argv[2]);
+  po::notify(vm);
+
+  const std::string inputFileName(vm["input-mesh"].as<std::string>());
+  const std::string outputFileName(vm["output-mesh"].as<std::string>());
 
   TCoordinate T[3] = {0.0, 0.0, 0.0};
-  T[0] = std::atof(argv[3]);
-  T[1] = std::atof(argv[4]);
-  T[2] = std::atof(argv[5]);
+  T[0] = vm["x"].as<double>();
+  T[1] = vm["y"].as<double>();
+  T[2] = vm["z"].as<double>();
 
   const auto reader = TReader::New();
   reader->SetFileName( inputFileName );
