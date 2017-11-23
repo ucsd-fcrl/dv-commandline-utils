41a312fa6af974c62924bf375142125ae75f3dcd
DVigneault <davis.vigneault@gmail.com>
2017-11-23 09:44:47 -0800

    boost::program_options for dv-permute-axes-3d

--- src/dv-permute-axes-3d.cxx
+++ src/dv-permute-axes-3d.cxx
@@ -10,7 +10,7 @@ namespace po = boost::program_options;
 
 const unsigned int Dimension = 3;
  
-typedef itk::Image<unsigned short, Dimension> TImage;
+typedef itk::Image<short, Dimension> TImage;
 
 typedef itk::ImageFileReader<TImage> TReader;
 typedef itk::PermuteAxesImageFilter<TImage> TPermute;
@@ -20,28 +20,39 @@ int
 main(int argc, char ** argv)
 {
 
-  if (argc != 6)
+  // Declare the supported options.
+  po::options_description description("Allowed options");
+  description.add_options()
+    ("help", "Print usage information.")
+    ("input-image",  po::value<std::string>()->required(), "Filename of the input image.")
+    ("output-image", po::value<std::string>()->required(), "Filename of the output image.")
+    ("x",            po::value<unsigned int>()->required(),      "Translation in the x direction.")
+    ("y",            po::value<unsigned int>()->required(),      "Translation in the y direction.")
+    ("z",            po::value<unsigned int>()->required(),      "Translation in the z direction.")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, description), vm);
+
+  if (vm.count("help"))
     {
-    std::cerr << "Usage: "
-              << argv[0]
-              << " <InputImage>"
-              << " <OutputImage>"
-              << " <DimX> <DimY> <DimZ>"
-              << std::endl;
-    return EXIT_FAILURE;
+    std::cout << description << '\n';
+    return EXIT_SUCCESS;
     }
 
-  const std::string IImage = argv[1];
-  const std::string OImage = argv[2];
+  po::notify(vm);
+
+  const std::string IImage = vm["input-image"].as<std::string>();
+  const std::string OImage = vm["output-image"].as<std::string>();
 
   itk::FixedArray<unsigned int, Dimension> order;
-  order[0] = std::atoi(argv[3]);
-  order[1] = std::atoi(argv[4]);
-  order[2] = std::atoi(argv[5]);
+  order[0] = vm["x"].as<unsigned int>();
+  order[1] = vm["y"].as<unsigned int>();
+  order[2] = vm["z"].as<unsigned int>();
 
   const auto reader = TReader::New();
   reader->SetFileName( IImage );
-  
+
   const auto permute = TPermute::New();
   permute->SetInput( reader->GetOutput() );
   permute->SetOrder(order);
