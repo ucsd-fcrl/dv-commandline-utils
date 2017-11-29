d573f6589a644fb1e5b99afa53a4e7033153978e
Davis Marc Vigneault <davis.vigneault@gmail.com>
2017-11-29 14:46:06 -0800

    ms => microseconds

--- src/dv-binarize-mesh.cxx
+++ src/dv-binarize-mesh.cxx
@@ -12,13 +12,15 @@ using ImagePixelType = unsigned char;
 int main(int argc, char** argv)
 {
 
+  std::string iMesh, rImage, oImage;
+
   // Declare the supported options.
   po::options_description description("Allowed options");
   description.add_options()
     ("help", "Print usage information.")
-    ("input-mesh",      po::value<std::string>()->required(), "Filename of the input mesh.")
-    ("reference-image", po::value<std::string>()->required(), "Filename of the reference image.")
-    ("output-image",    po::value<std::string>()->required(), "Filename of the output image.")
+    ("input-mesh",      po::value<std::string>(&iMesh)->required(), "Filename of the input mesh.")
+    ("reference-image", po::value<std::string>(&rImage)->required(), "Filename of the reference image.")
+    ("output-image",    po::value<std::string>(&oImage)->required(), "Filename of the output image.")
   ;
 
   po::variables_map vm;
@@ -32,7 +34,9 @@ int main(int argc, char** argv)
 
   po::notify(vm);
 
-  dv::BinarizeMesh< Dimension, double, ImagePixelType >(vm);
+  dv::BinarizeMesh< Dimension, double, ImagePixelType >(iMesh,
+                                                        rImage,
+                                                        oImage);
 
   return EXIT_SUCCESS;
 }
