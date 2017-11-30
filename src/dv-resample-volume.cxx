6263c866931c389d08c21345a8efabee247fed6b
Davis Marc Vigneault <davis.vigneault@gmail.com>
2017-11-30 09:22:57 -0800

    Resample Volume: Arbitrary Pixel Type

--- src/dv-resample-volume.cxx
+++ src/dv-resample-volume.cxx
@@ -3,33 +3,9 @@
 
 namespace po = boost::program_options;
 
-// ITK
-#include <itkImage.h>
-#include <itkIdentityTransform.h>
-#include <itkResampleImageFilter.h>
-#include <itkImageFileReader.h>
-#include <itkImageFileWriter.h>
-#include <itkFileTools.h>
 
-#include <itkNearestNeighborInterpolateImageFunction.h>
-#include <itkLinearInterpolateImageFunction.h>
-#include <itkBSplineInterpolateImageFunction.h>
-
-namespace po = boost::program_options;
-
-// Typedefs
-const unsigned int Dimension = 3;
-
-using PixelType = signed short;
-using ImageType = itk::Image< PixelType, Dimension >;
-using ReaderType = itk::ImageFileReader< ImageType >;
-using WriterType = itk::ImageFileWriter< ImageType >;
-using TransformType = itk::IdentityTransform< double, Dimension >;
-using ResampleType = itk::ResampleImageFilter< ImageType, ImageType >;
-
-using NNInterpolateType = itk::NearestNeighborInterpolateImageFunction< ImageType >;
-using LNInterpolateType = itk::LinearInterpolateImageFunction< ImageType >;
-using BSInterpolateType = itk::BSplineInterpolateImageFunction< ImageType >;
+#include <dvResampleVolume.h>
+#include <dvReadImageIOBase.h>
 
 int
 main( int argc, char* argv[] )
@@ -56,60 +32,48 @@ main( int argc, char* argv[] )
 
   po::notify(vm);
 
-  const auto reader = ReaderType::New();
-  const auto resample = ResampleType::New();
-  const auto writer = WriterType::New();
+  const std::string IImage        = vm["input-image"].as<std::string>();
+  const std::string OImage        = vm["output-image"].as<std::string>();
+  const double spacing            = vm["spacing"].as<double>();
+  const unsigned int interpolator = vm["interpolator"].as<unsigned int>();
 
-  reader->SetFileName( vm["input-image"].as<std::string>() );
-  reader->Update();
-
-  const auto image = ImageType::New();
-  image->Graft( reader->GetOutput() );
-
-  ImageType::SpacingType outputSpacing;
-  outputSpacing.Fill( vm["spacing"].as<double>() );
-  auto inputSize = image->GetLargestPossibleRegion().GetSize();
-  auto inputSpacing = image->GetSpacing();
-  ImageType::SizeType outputSize;
-  for (unsigned int i = 0; i < Dimension; ++i)
+  switch (dv::ReadImageIOBase(IImage)->GetComponentType())
     {
-    outputSize[i] = std::floor(double(inputSize[i]) * inputSpacing[i] / outputSpacing[i]);
-    }
-
-  resample->SetTransform( TransformType::New() );
-  resample->SetOutputStartIndex( image->GetLargestPossibleRegion().GetIndex() );
-  resample->SetOutputOrigin( image->GetOrigin() );
-  resample->SetOutputDirection( image->GetDirection() );
-  resample->SetOutputSpacing( outputSpacing );
-  resample->SetSize( outputSize );
-  resample->SetInput( image );
-
-  switch (vm["interpolator"].as<unsigned int>())
-    {
-    case 0:
-      {
-      const auto interp = NNInterpolateType::New();
-      resample->SetInterpolator( interp );
+    case itk::ImageIOBase::UCHAR:
+      dv::ResampleVolume<3, unsigned char>(IImage, OImage, spacing, interpolator);
       break;
-      }
-    case 1:
-      {
-      const auto interp = LNInterpolateType::New();
-      resample->SetInterpolator( interp );
+    case itk::ImageIOBase::CHAR:
+      dv::ResampleVolume<3, char>(IImage, OImage, spacing, interpolator);
       break;
-      }
+    case itk::ImageIOBase::USHORT:
+      dv::ResampleVolume<3, unsigned short>(IImage, OImage, spacing, interpolator);
+      break;
+    case itk::ImageIOBase::SHORT:
+      dv::ResampleVolume<3, short>(IImage, OImage, spacing, interpolator);
+      break;
+    case itk::ImageIOBase::UINT:
+      dv::ResampleVolume<3, unsigned int>(IImage, OImage, spacing, interpolator);
+      break;
+    case itk::ImageIOBase::INT:
+      dv::ResampleVolume<3, int>(IImage, OImage, spacing, interpolator);
+      break;
+    case itk::ImageIOBase::ULONG:
+      dv::ResampleVolume<3, unsigned long>(IImage, OImage, spacing, interpolator);
+      break;
+    case itk::ImageIOBase::LONG:
+      dv::ResampleVolume<3, long>(IImage, OImage, spacing, interpolator);
+      break;
+    case itk::ImageIOBase::FLOAT:
+      dv::ResampleVolume<3, float>(IImage, OImage, spacing, interpolator);
+      break;
+    case itk::ImageIOBase::DOUBLE:
+      dv::ResampleVolume<3, double>(IImage, OImage, spacing, interpolator);
+      break;
     default:
-      {
-      const auto interp = BSInterpolateType::New();
-      interp->SetSplineOrder( vm["interpolator"].as<unsigned int>() );
-      resample->SetInterpolator( interp );
+      std::cerr << "ERROR: Unrecognized pixel type." << std::endl;
+      return EXIT_FAILURE;
       break;
-      }
     }
-
-  writer->SetInput( resample->GetOutput() );
-  writer->SetFileName( vm["output-image"].as<std::string>() );
-  writer->Update();
 
   return EXIT_SUCCESS;
 
