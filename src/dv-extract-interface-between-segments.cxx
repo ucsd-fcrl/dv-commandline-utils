810c882f1eb5ba9ec274201504d6f9d571e23079
Davis Marc Vigneault <davis.vigneault@gmail.com>
2018-01-11 09:53:35 -0800

    Add Interface Extration Tool

---
+++ src/dv-extract-interface-between-segments.cxx
@@ -0,0 +1,57 @@
+// Boost
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+// Custom
+#include <dvExtractInterfaceBetweenSegments.h>
+
+// STD
+#include <vector>
+
+const unsigned int Dimension = 3;
+using TCoordinate = float;
+using TPixel = unsigned char;
+
+int
+main( int argc, char* argv[] )
+{
+
+  // Declare the supported options.
+  po::options_description description("Allowed options");
+  description.add_options()
+    ("help", "Print usage information.")
+    ("input-image", po::value<std::string>()->required(),       "Filename of the segmentation image.")
+    ("output-mesh", po::value<std::string>()->required(),       "Filename of the output mesh.")
+    ("labels-1", po::value<std::vector<short>>()->multitoken(), "First label set to extract.")
+    ("labels-2", po::value<std::vector<short>>()->multitoken(), "Second label set to extract.")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, description), vm);
+
+  if (vm.count("help"))
+    {
+    std::cout << description << '\n';
+    return EXIT_SUCCESS;
+    }
+
+  po::notify(vm);
+
+  const std::string inputFileName(vm["input-image"].as<std::string>());
+  const std::string outputFileName(vm["output-mesh"].as<std::string>());
+  const std::vector<short> labels_vector_1 = vm["labels-1"].as<std::vector<short>>();
+  const std::vector<short> labels_vector_2 = vm["labels-2"].as<std::vector<short>>();
+
+  const std::set<TPixel> labels_set_1(labels_vector_1.begin(), labels_vector_1.end());
+  const std::set<TPixel> labels_set_2(labels_vector_2.begin(), labels_vector_2.end());
+
+
+  //
+  // make Set from labels given
+  //
+  dv::ExtractInterfaceBetweenSegments<Dimension, TPixel, TCoordinate>(inputFileName, outputFileName, labels_set_1, labels_set_2);
+
+  return EXIT_SUCCESS;
+
+}
---
+++ src/includes/dvExtractInterfaceBetweenSegments.h
@@ -0,0 +1,141 @@
+#ifndef dv_ExtractInterfaceBetweenSegments_h
+#define dv_ExtractInterfaceBetweenSegments_h
+
+// ITK
+#include <itkMesh.h>
+#include <itkImageFileReader.h>
+#include <itkMeshFileWriter.h>
+#include <itkSTLMeshIO.h>
+#include <itkBinaryMask3DMeshSource.h>
+#include <itkStatisticsImageFilter.h>
+#include <itkMacro.h>
+#include <itkBinaryDilateImageFilter.h>
+#include <itkBinaryBallStructuringElement.h>
+#include <itkAndImageFilter.h>
+
+// Custom
+#include "itkExtractLabelsImageFilter.h"
+
+namespace dv
+{
+
+template<unsigned int Dimension, typename TPixel, typename TCoordinate>
+void
+ExtractInterfaceBetweenSegments(const std::string &IImage, const std::string &OMesh, const std::set<TPixel> &labels_1, const std::set<TPixel> &labels_2)
+{
+
+  using TImage = itk::Image< TPixel, Dimension >;
+  using TMesh   = itk::Mesh< TCoordinate, Dimension >;
+
+  using ReaderType = itk::ImageFileReader< TImage >;
+  using FilterType = itk::ExtractLabelsImageFilter<TImage>;
+  using TMeshSource = itk::BinaryMask3DMeshSource< TImage, TMesh >;
+  using TStats = itk::StatisticsImageFilter< TImage >;
+  using TBall = itk::BinaryBallStructuringElement< TPixel, Dimension>;
+  using TDilate = itk::BinaryDilateImageFilter< TImage, TImage, TBall >;
+  using TAnd = itk::AndImageFilter< TImage >;
+
+  using TWriter = itk::MeshFileWriter< TMesh >;
+
+  //
+  // Reader
+  //
+
+  const auto reader = ReaderType::New();
+  reader->SetFileName( IImage );
+
+  //
+  // make unary filter which checks if pixels are in set
+  //
+  const auto filter_1 = FilterType::New();
+  filter_1->SetLabels( labels_1 );
+  filter_1->SetInput( reader->GetOutput() );
+  filter_1->Update();
+
+  const auto binary_1 = TImage::New();
+  binary_1->Graft( filter_1->GetOutput() );
+  binary_1->DisconnectPipeline();
+
+  const auto filter_2 = FilterType::New();
+  filter_2->SetLabels( labels_2 );
+  filter_2->SetInput( reader->GetOutput() );
+  filter_2->Update();
+
+  const auto binary_2 = TImage::New();
+  binary_2->Graft( filter_2->GetOutput() );
+  binary_2->DisconnectPipeline();
+
+  // Check
+
+    {
+    const auto stats = TStats::New();
+    stats->SetInput( binary_1 );
+    stats->Update();
+    itkAssertOrThrowMacro(0 < stats->GetSum(), "Segmentation image 1 is empty.");
+    }
+
+    {
+    const auto stats = TStats::New();
+    stats->SetInput( binary_2 );
+    stats->Update();
+    itkAssertOrThrowMacro(0 < stats->GetSum(), "Segmentation image 2 is empty.");
+    }
+
+  // Dilate
+
+  TBall ball;
+  ball.SetRadius( 1 );
+  ball.CreateStructuringElement();
+
+  const auto dilate_1 = TDilate::New();
+  dilate_1->SetInput( binary_1 );
+  dilate_1->SetKernel( ball );
+  dilate_1->SetDilateValue( 1 );
+
+  const auto dilate_2 = TDilate::New();
+  dilate_2->SetInput( binary_2 );
+  dilate_2->SetKernel( ball );
+  dilate_2->SetDilateValue( 1 );
+
+  // Intersection
+  const auto intersection = TAnd::New();
+  intersection->SetInput1( dilate_1->GetOutput() );
+  intersection->SetInput2( dilate_2->GetOutput() );
+  intersection->Update();
+
+    {
+    const auto stats = TStats::New();
+    stats->SetInput( intersection->GetOutput() );
+    stats->Update();
+    itkAssertOrThrowMacro(0 < stats->GetSum(), "Intersection image is empty.");
+    }
+  
+  //
+  // Meshing
+  //
+  const auto meshSource = TMeshSource::New();
+  const TPixel objectValue = static_cast<TPixel>( 1 );
+  meshSource->SetObjectValue( objectValue );
+  meshSource->SetInput( intersection->GetOutput() );
+
+  //
+  // Writer
+  //
+
+  const auto writer = TWriter::New();
+  writer->SetInput( meshSource->GetOutput() );
+  writer->SetFileName( OMesh );
+
+  const auto o_ext = OMesh.substr( OMesh.size() - 3, 3);
+  if (o_ext == "stl" || o_ext == "STL")
+  {
+    writer->SetMeshIO( itk::STLMeshIO::New() );
+  }
+
+  writer->Update();
+
+}
+
+}
+
+#endif
