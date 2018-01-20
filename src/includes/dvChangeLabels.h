866b8406ecfa127205e59bbb2343036a24285007
DVigneault <davis.vigneault@gmail.com>
2018-01-20 14:43:12 -0800

    Add Label Map Thing

---
+++ src/includes/dvChangeLabels.h
@@ -0,0 +1,55 @@
+#ifndef dv_ConvertLabelsToMesh_h
+#define dv_ConvertLabelsToMesh_h
+
+// ITK
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+
+// Custom
+#include <itkChangeLabelsImageFilter.h>
+
+// STD
+#include <map>
+
+namespace dv
+{
+
+template<unsigned int Dimension, typename TPixel>
+void
+ChangeLabels(const std::string &IImage, const std::map<TPixel, TPixel> &LabelMap, const std::string &OImage)
+{
+
+  using TImage  = itk::Image< TPixel, Dimension >;
+  using TReader = itk::ImageFileReader< TImage >;
+  using TFilter = itk::ChangeLabelsImageFilter< TImage >;
+  using TWriter = itk::ImageFileWriter< TImage >;
+
+  //
+  // Reader
+  //
+
+  const auto reader = TReader::New();
+  reader->SetFileName( IImage );
+
+  //
+  // Map Labels
+  //
+
+  const auto filter = TFilter::New();
+  filter->SetLabelMap( LabelMap );
+  filter->SetInput( reader->GetOutput() );
+
+  //
+  // Writer
+  //
+
+  const auto writer = TWriter::New();
+  writer->SetInput( filter->GetOutput() );
+  writer->SetFileName( OImage );
+  writer->Update();
+
+}
+
+}
+
+#endif
