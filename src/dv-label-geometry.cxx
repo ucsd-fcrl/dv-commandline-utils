0df08c05510105bba1675e63ebf35593adb83d35
DVigneault <davis.vigneault@gmail.com>
2018-03-24 20:41:40 -0700

    Label Geometry Filter

---
+++ src/dv-label-geometry.cxx
@@ -0,0 +1,87 @@
+// Boost
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+// Custom
+#include <dvReadImageIOBase.h>
+#include <dvLabelGeometry.h>
+
+const unsigned int Dimension = 3;
+
+int
+main( int argc, char** argv )
+{
+
+  // Declare the supported options.
+  po::options_description description("Allowed options");
+  description.add_options()
+    ("help", "Print usage information.")
+    ("input-image",  po::value<std::string>()->required(),          "Filename of the input image.")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, description), vm);
+
+  if (vm.count("help"))
+    {
+    std::cout << description << '\n';
+    return EXIT_SUCCESS;
+    }
+
+  po::notify(vm);
+
+  const std::string IImage(vm["input-image"].as<std::string>());
+
+  switch (dv::ReadImageIOBase(IImage)->GetComponentType())
+    {
+    case itk::ImageIOBase::UCHAR:
+      {
+      dv::LabelGeometry<3, unsigned char>(IImage);
+      break;
+      }
+    case itk::ImageIOBase::CHAR:
+      {
+      dv::LabelGeometry<3, char>(IImage);
+      break;
+      }
+    case itk::ImageIOBase::USHORT:
+      {
+      dv::LabelGeometry<3, unsigned short>(IImage);
+      break;
+      }
+    case itk::ImageIOBase::SHORT:
+      {
+      dv::LabelGeometry<3, short>(IImage);
+      break;
+      }
+    case itk::ImageIOBase::UINT:
+      {
+      dv::LabelGeometry<3, unsigned int>(IImage);
+      break;
+      }
+    case itk::ImageIOBase::INT:
+      {
+      dv::LabelGeometry<3, int>(IImage);
+      break;
+      }
+    case itk::ImageIOBase::ULONG:
+      {
+      dv::LabelGeometry<3, unsigned long>(IImage);
+      break;
+      }
+    case itk::ImageIOBase::LONG:
+      {
+      dv::LabelGeometry<3, long>(IImage);
+      break;
+      }
+    default:
+      std::cerr << "ERROR: Unrecognized pixel type." << std::endl;
+      return EXIT_FAILURE;
+      break;
+    }
+
+  return EXIT_SUCCESS;
+
+}
+
---
+++ src/includes/dvLabelGeometry.h
@@ -0,0 +1,58 @@
+#ifndef dv_LabelGeometry_h
+#define dv_LabelGeometry_h
+
+// ITK
+#include <itkImageFileReader.h>
+#include <itkLabelGeometryImageFilter.h>
+#include <itkImageFileWriter.h>
+
+// Custom
+#include <dvImageToSet.h>
+
+// STD
+#include <map>
+
+namespace dv
+{
+
+template<unsigned int Dimension, typename TPixel>
+void
+LabelGeometry(const std::string &IImage)
+{
+
+  using TImage  = itk::Image< TPixel, Dimension >;
+  using TReader = itk::ImageFileReader< TImage >;
+  using TFilter = itk::LabelGeometryImageFilter< TImage >;
+
+  //
+  // Calculate Stats
+  //
+
+  const auto reader = TReader::New();
+  reader->SetFileName( IImage );
+
+  const auto filter = TFilter::New();
+  filter->SetInput( reader->GetOutput() );
+  filter->Update();
+
+  //
+  // Calculate Label Stats
+  //
+
+  const auto labels = dv::ImageToSet<Dimension, TPixel>( reader->GetOutput() );
+
+  for (const auto &l : labels)
+    {
+    std::cout << "Volume,"       << l << ',' << filter->GetVolume( l ) << std::endl;
+    std::cout << "CentroidX,"    << l << ',' << filter->GetCentroid( l )[0] << std::endl;
+    std::cout << "CentroidY,"    << l << ',' << filter->GetCentroid( l )[1] << std::endl;
+    std::cout << "CentroidZ,"    << l << ',' << filter->GetCentroid( l )[2] << std::endl;
+    std::cout << "Eccentricity," << l << ',' << filter->GetEccentricity( l ) << std::endl;
+    std::cout << "Elongation,"   << l << ',' << filter->GetElongation( l ) << std::endl;
+    }
+
+}
+
+}
+
+#endif
