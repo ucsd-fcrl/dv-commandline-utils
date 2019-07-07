1a24ea2513829062b7f8aee689cd14bc74059e33
Davis Marc Vigneault <davis.vigneault@gmail.com>
2019-07-07 11:01:30 -0700

    Remove dead code

--- src/includes/dvCalculateSurfaceAreas.h
+++ src/includes/dvCalculateSurfaceAreas.h
@@ -1,14 +1,12 @@
 #ifndef dvCalculateSurfaceAreas_h
 #define dvCalculateSurfaceAreas_h
 
-//#include <vnl/vnl_vector.h>
 #include <vector>
 #include <vtkPolyData.h>
 
 namespace dv
 {
 
-//vnl_vector<double>
 std::vector<double>
 CalculateSurfaceAreas(vtkPolyData* mesh);
 
