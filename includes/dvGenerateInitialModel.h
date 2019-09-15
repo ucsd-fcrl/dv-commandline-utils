ca98b2392a6d470a602223031705de7e1b6c9f58
Davis Marc Vigneault <davis.vigneault@gmail.com>
2019-09-15 00:35:25 -0400

    Separate GenerateInitialModel into Independent Function

---
+++ includes/dvGenerateInitialModel.h
@@ -0,0 +1,104 @@
+#ifndef dv_GenerateInitialModel_h
+#define dv_GenerateInitialModel_h
+
+// STD
+#include <string>
+
+// ITK
+#include <itkDefaultDynamicMeshTraits.h>
+#include <itkMesh.h>
+#include <itkQuadEdgeMesh.h>
+#include <itkMeshFileReader.h>
+#include <itkMeshFileWriter.h>
+#include <itkDelaunayConformingQuadEdgeMeshFilter.h>
+#include <itkAdditiveGaussianNoiseQuadEdgeMeshFilter.h>
+#include <itkQuadEdgeMeshDecimationCriteria.h>
+#include <itkSquaredEdgeLengthDecimationQuadEdgeMeshFilter.h>
+#include <itkConnectedRegionsMeshFilter.h>
+#include <itkLoopTriangleCellSubdivisionQuadEdgeMeshFilter.h>
+
+// DV
+#include <dvDeleteIsolatedPoints.h>
+#include <dvSqueezePointsIds.h>
+#include <dvRefineValenceThreeVertices.h>
+#include <dvMeshToQuadEdgeMesh.h>
+
+namespace dv
+{
+
+void
+GenerateInitialModel(
+  const std::string &inputMeshName,
+  const std::string &outputMeshName,
+  const unsigned int &count,
+  const double &sigma
+  )
+{
+
+const unsigned int Dimension = 3;
+using TCoordinate = float;
+ 
+using TMeshTraits = itk::DefaultDynamicMeshTraits< TCoordinate >;
+using TMesh       = itk::Mesh< TCoordinate, Dimension, TMeshTraits >;
+using TReader     = itk::MeshFileReader< TMesh >;
+using TWriter     = itk::MeshFileWriter< TMesh >;
+
+using TQEMesh       = itk::QuadEdgeMesh< TCoordinate, Dimension >;
+using TQEReader     = itk::MeshFileReader< TQEMesh >;
+using TQEWriter     = itk::MeshFileWriter< TQEMesh >;
+
+using TConnected = itk::ConnectedRegionsMeshFilter< TMesh, TMesh >;
+using TNoise = itk::AdditiveGaussianNoiseQuadEdgeMeshFilter< TQEMesh >;
+using TCriterion  = itk::NumberOfFacesCriterion< TQEMesh >;
+using TDecimation = itk::SquaredEdgeLengthDecimationQuadEdgeMeshFilter< TQEMesh,
+                                                              TQEMesh,
+                                                              TCriterion >;
+using TLoop = itk::LoopTriangleCellSubdivisionQuadEdgeMeshFilter< TQEMesh, TQEMesh >;
+using TDelaunay = itk::DelaunayConformingQuadEdgeMeshFilter< TQEMesh >;
+
+const auto reader = TReader::New();
+reader->SetFileName( inputMeshName );
+
+const auto connected = TConnected::New();
+connected->SetInput( reader->GetOutput() );
+connected->SetExtractionModeToLargestRegion();
+connected->Update();
+
+const auto mesh = TMesh::New();
+mesh->Graft( connected->GetOutput() );
+mesh->DisconnectPipeline();
+
+dv::DeleteIsolatedPoints<TMesh>( mesh );
+dv::SqueezePointsIds<TMesh>( mesh );
+
+const auto qemesh = TQEMesh::New();
+dv::MeshToQuadEdgeMesh< TMesh, TQEMesh >( mesh, qemesh );
+
+const auto noise = TNoise::New();
+noise->SetInput( qemesh );
+noise->SetSigma( sigma );
+
+const auto criterion = TCriterion::New();
+const auto decimate = TDecimation::New();
+
+criterion->SetNumberOfElements( count );
+
+decimate->SetInput( noise->GetOutput() );
+decimate->SetCriterion( criterion );
+
+const auto delaunay = TDelaunay::New();
+delaunay->SetInput( decimate->GetOutput() );
+
+const auto loop = TLoop::New();
+loop->SetInput( delaunay->GetOutput() );
+
+const auto writer = TQEWriter::New();
+writer->SetInput( loop->GetOutput() );
+writer->SetFileName( outputMeshName );
+writer->Update();
+
+}
+
+}
+
+#endif
--- utils/dv-generate-initial-model.cxx
+++ utils/dv-generate-initial-model.cxx
@@ -3,53 +3,8 @@
 
 namespace po = boost::program_options;
 
-// ITK
-#include <itkDefaultDynamicMeshTraits.h>
-#include <itkMesh.h>
-#include <itkQuadEdgeMesh.h>
-#include <itkMeshFileReader.h>
-#include <itkMeshFileWriter.h>
-
-// => Connected
-// => Delete Unassigned
-// => Noise
-// => Decimate
-// => Connected
-// => Delete Unassigned
-// => Refine
-// => Loop
-
-#include <itkDelaunayConformingQuadEdgeMeshFilter.h>
-#include <itkAdditiveGaussianNoiseQuadEdgeMeshFilter.h>
-#include <itkQuadEdgeMeshDecimationCriteria.h>
-#include <itkSquaredEdgeLengthDecimationQuadEdgeMeshFilter.h>
-#include <itkConnectedRegionsMeshFilter.h>
-#include <dvDeleteIsolatedPoints.h>
-#include <dvSqueezePointsIds.h>
-#include <dvRefineValenceThreeVertices.h>
-#include <itkLoopTriangleCellSubdivisionQuadEdgeMeshFilter.h>
-#include <dvMeshToQuadEdgeMesh.h>
- 
-const unsigned int Dimension = 3;
-using TCoordinate = float;
- 
-using TMeshTraits = itk::DefaultDynamicMeshTraits< TCoordinate >;
-using TMesh       = itk::Mesh< TCoordinate, Dimension, TMeshTraits >;
-using TReader     = itk::MeshFileReader< TMesh >;
-using TWriter     = itk::MeshFileWriter< TMesh >;
-
-using TQEMesh       = itk::QuadEdgeMesh< TCoordinate, Dimension >;
-using TQEReader     = itk::MeshFileReader< TQEMesh >;
-using TQEWriter     = itk::MeshFileWriter< TQEMesh >;
-
-using TConnected = itk::ConnectedRegionsMeshFilter< TMesh, TMesh >;
-using TNoise = itk::AdditiveGaussianNoiseQuadEdgeMeshFilter< TQEMesh >;
-using TCriterion  = itk::NumberOfFacesCriterion< TQEMesh >;
-using TDecimation = itk::SquaredEdgeLengthDecimationQuadEdgeMeshFilter< TQEMesh,
-                                                              TQEMesh,
-                                                              TCriterion >;
-using TLoop = itk::LoopTriangleCellSubdivisionQuadEdgeMeshFilter< TQEMesh, TQEMesh >;
-using TDelaunay = itk::DelaunayConformingQuadEdgeMeshFilter< TQEMesh >;
+// DV
+#include <dvGenerateInitialModel.h>
 
 int
 main( int argc, char ** argv )
@@ -81,46 +36,21 @@ main( int argc, char ** argv )
   const unsigned int count = vm["count"].as<unsigned int>();
   const double sigma = vm["sigma"].as<double>();
 
-  const auto reader = TReader::New();
-  reader->SetFileName( inputMeshName );
+// => Connected
+// => Delete Unassigned
+// => Noise
+// => Decimate
+// => Connected
+// => Delete Unassigned
+// => Refine
+// => Loop
 
-  const auto connected = TConnected::New();
-  connected->SetInput( reader->GetOutput() );
-  connected->SetExtractionModeToLargestRegion();
-  connected->Update();
-
-  const auto mesh = TMesh::New();
-  mesh->Graft( connected->GetOutput() );
-  mesh->DisconnectPipeline();
-
-  dv::DeleteIsolatedPoints<TMesh>( mesh );
-  dv::SqueezePointsIds<TMesh>( mesh );
-
-  const auto qemesh = TQEMesh::New();
-  dv::MeshToQuadEdgeMesh< TMesh, TQEMesh >( mesh, qemesh );
-
-  const auto noise = TNoise::New();
-  noise->SetInput( qemesh );
-  noise->SetSigma( sigma );
-
-  const auto criterion = TCriterion::New();
-  const auto decimate = TDecimation::New();
-
-  criterion->SetNumberOfElements( count );
-
-  decimate->SetInput( noise->GetOutput() );
-  decimate->SetCriterion( criterion );
-
-  const auto delaunay = TDelaunay::New();
-  delaunay->SetInput( decimate->GetOutput() );
-
-  const auto loop = TLoop::New();
-  loop->SetInput( delaunay->GetOutput() );
-
-  const auto writer = TQEWriter::New();
-  writer->SetInput( loop->GetOutput() );
-  writer->SetFileName( outputMeshName );
-  writer->Update();
+  dv::GenerateInitialModel(
+    inputMeshName,
+    outputMeshName,
+    count,
+    sigma
+    );
 
   return EXIT_SUCCESS;
  
