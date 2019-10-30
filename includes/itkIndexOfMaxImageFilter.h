59f169e03f907776b2ed3eb2524aa4f1e8625836
Laura Xu <laxu@eng.ucsd.edu>
2019-10-30 16:41:12 -0700

    resample

--- includes/itkIndexOfMaxImageFilter.h
+++ includes/itkIndexOfMaxImageFilter.h
@@ -4,66 +4,73 @@
 #include <itkImage.h>
 #include <itkUnaryFunctorImageFilter.h>
 
-namespace itk {
-
-namespace Functor {
-
-template<typename TInput, typename TOutput = TInput>
-class IndexOfMaxFunctor
+namespace itk
 {
-public:
-  IndexOfMaxFunctor(){};
-  ~IndexOfMaxFunctor(){};
 
-  inline TOutput operator()(const TInput& A)
+  namespace Functor
   {
-    // TInput (PixelType) is a vector of e.g. doubles
-    // TOutput (PixelType) is an int (label)
 
-    int maxIndex = 0;
-    double maxVal = 0;
-    for (int i = 0; i < A.Size(); i++) {
-      if (A.GetElement(i) > maxVal && A.GetElement(i) >= 0.25) {
-        maxVal = A.GetElement(i);
-        maxIndex = i + 1;
+    template< typename TInput, typename TOutput = TInput>
+    class IndexOfMaxFunctor
+    {
+    public:
+      
+      IndexOfMaxFunctor() {};
+      ~IndexOfMaxFunctor() {};
+
+      /**
+       * Returns index + 1 (label) of maximum value greater than 0.5, otherwise 0
+       * TInput - input image PixelType (vector)
+       * TOutput - output image PixelType (int)
+       **/
+      inline TOutput operator()( const TInput & A )
+      {
+
+        int maxIndex = 0;
+        double maxVal = 0;
+        for (int i = 0; i < A.Size(); i++) {
+          if (A.GetElement(i) > maxVal && A.GetElement(i) > 0.5) {
+            maxVal = A.GetElement(i);
+            maxIndex = i + 1;
+          }
+        }
+        return static_cast<TOutput>( maxIndex );
       }
-    }
-    return static_cast<TOutput>(maxIndex);
-  }
-};
-};
 
-template<typename TInputImage, typename TOutputImage>
-class IndexOfMaxImageFilter
-  : public UnaryFunctorImageFilter<
-      TInputImage,
-      TOutputImage,
-      Functor::IndexOfMaxFunctor<typename TInputImage::PixelType,
-                                 typename TOutputImage::PixelType>>
-{
-public:
-  using Self = IndexOfMaxImageFilter;
-  using Superclass = UnaryFunctorImageFilter<
-    TInputImage,
-    TOutputImage,
-    Functor::IndexOfMaxFunctor<typename TInputImage::PixelType,
-                               typename TOutputImage::PixelType>>;
+    };
+  };
 
-  using Pointer = SmartPointer<Self>;
-  using ConstPointer = SmartPointer<const Self>;
+  template<typename TInputImage, typename TOutputImage>
+  class IndexOfMaxImageFilter
+    : public UnaryFunctorImageFilter<
+        TInputImage,
+        TOutputImage,
+        Functor::IndexOfMaxFunctor< typename TInputImage::PixelType, typename TOutputImage::PixelType> >
+  {
+  public:
 
-  itkNewMacro(Self);
-  itkTypeMacro(IndexOfMaxImageFilter, UnaryFunctorImageFilter);
+    using Self = IndexOfMaxImageFilter;
+    using Superclass = UnaryFunctorImageFilter< TInputImage,
+                                                TOutputImage,
+                                                Functor::IndexOfMaxFunctor< typename TInputImage::PixelType, typename TOutputImage::PixelType > >;
 
-protected:
-  IndexOfMaxImageFilter() {}
-  virtual ~IndexOfMaxImageFilter() {}
+    using Pointer = SmartPointer< Self >;
+    using ConstPointer = SmartPointer< const Self >;
 
-private:
-  IndexOfMaxImageFilter(const Self&); // purposely not implemented
-  void operator=(const Self&);        // purposely not implemented
+    itkNewMacro( Self );
+    itkTypeMacro( IndexOfMaxImageFilter, UnaryFunctorImageFilter );
 
-}; // end of class
+  protected:
+
+    IndexOfMaxImageFilter() {}
+    virtual ~IndexOfMaxImageFilter() {}
+
+  private:
+
+    IndexOfMaxImageFilter( const Self & ); // purposely not implemented
+    void operator=(  const Self & );      // purposely not implemented
+
+  };   // end of class
 };
 
 #endif
