e1956bc8eec6a1f9295ee1fa9bf9f79ad180d0eb
Davis Marc Vigneault <davis.vigneault@gmail.com>
2020-07-05 06:28:23 -0700

    Add Subdivision To Cuberille Test

--- tests/src/dvCuberilleQuadEdgeMeshTest.cxx
+++ tests/src/dvCuberilleQuadEdgeMeshTest.cxx
@@ -4,6 +4,12 @@
 #include <itkConstantPadImageFilter.h>
 #include <itkQuadEdgeMesh.h>
 #include <itkCuberilleImageToMeshFilter.h>
+#include <itkLoopTriangleCellSubdivisionQuadEdgeMeshFilter.h>
+#include <itkConnectedComponentImageFilter.h>
+
+// Custom
+#include <dvITKTriangleMeshToVTKPolyData.h>
+#include <dvQuickViewSideBySidePolyData.h>
 
 int main() {
 
@@ -13,9 +19,11 @@ int main() {
   using TImage = itk::Image<TPixel, Dimension>;
   using TMesh = itk::QuadEdgeMesh<TCoordinate, Dimension>;
   using TPad = itk::ConstantPadImageFilter<TImage, TImage>;
+  using TConnected = itk::ConnectedComponentImageFilter<TImage, TImage>;
   using TExtract = itk::CuberilleImageToMeshFilter< TImage, TMesh >;
+  using TLoop = itk::LoopTriangleCellSubdivisionQuadEdgeMeshFilter< TMesh >;
 
-  for (size_t mask = 0; mask < std::pow(2, 8); ++mask) {
+  for (size_t mask = 255; mask < std::pow(2, 8); ++mask) {
 
     std::bitset<8> bitmask(mask);
 
@@ -48,12 +56,31 @@ int main() {
     pad->SetPadLowerBound(padding);
     pad->SetConstant(static_cast<TPixel>(0));
 
+    const auto connected = TConnected::New();
+    connected->SetInput( pad->GetOutput() );
+
     const auto extract = TExtract::New();
-    extract->SetInput( pad->GetOutput() );
+    extract->SetInput( connected->GetOutput() );
     extract->GenerateTriangleFacesOn();
     extract->ProjectVerticesToIsoSurfaceOff();
     extract->SavePixelAsCellDataOn();
     ITK_TRY_EXPECT_NO_EXCEPTION(extract->Update());
+
+    const auto loop0 = TLoop::New();
+    loop0->SetInput( extract->GetOutput() );
+    const auto loop1 = TLoop::New();
+    loop1->SetInput( loop0->GetOutput() );
+    const auto loop2 = TLoop::New();
+    loop2->SetInput( loop1->GetOutput() );
+    const auto loop3 = TLoop::New();
+    loop3->SetInput( loop2->GetOutput() );
+    ITK_TRY_EXPECT_NO_EXCEPTION(loop3->Update());
+
+    {
+    const auto poly_data_i = dv::ITKTriangleMeshToVTKPolyData< TMesh >( extract->GetOutput() );
+    const auto poly_data_o = dv::ITKTriangleMeshToVTKPolyData< TMesh >( loop3->GetOutput() );
+    dv::QuickViewSideBySidePolyData( poly_data_i, poly_data_o );
+    }
 
   }
 
